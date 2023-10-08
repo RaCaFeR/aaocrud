e2c8d0670079d723d353749be2479a8674506fcd
Helio <helioprorep@gmail.com>
2023-10-08 14:25:41 -0300

    test

--- src/main/java/me/crud/database/ProductManager.java
+++ src/main/java/me/crud/database/ProductManager.java
@@ -1,4 +1,18 @@
 package me.crud.database;
 
+import me.crud.model.Product;
+
+import java.util.HashMap;
+
 public class ProductManager {
+    private ProductManager instance;
+    private HashMap<Integer, Product> products;
+
+    public HashMap<Integer, Product> getProducts() {
+        return products;
+    }
+
+    public ProductManager getInstance() {
+        return instance;
+    }
 }
